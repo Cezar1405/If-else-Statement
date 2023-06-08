a57eb3f2010c597597b1b98043efbd10e215fc85
Cezar1405 <cicg.14.05@gmail.com>
2023-06-08 23:07:29 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        //Define a variable called myName and assign your name to it
+        String myname = "Cesar";
+        //Using the if-else statement, print if the number of letters in your name is even or odd
+        if (myname.length() % 2 == 0){
+            System.out.println("El numero de letras de mi nombre es par");
+        }else{
+            System.out.println("El numero de letras de mi nombre es impar");
+        }
+    }
+}
